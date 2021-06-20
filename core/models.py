be8509968da2d0be43285ed86ced20c580576296
Jhonnylibra <jhonnylibra@gmail.com>
2021-06-19 22:57:57 -0300

    Criada listagem de eventos

--- core/models.py
+++ core/models.py
@@ -14,5 +14,13 @@ class Evento(models.Model):
     def __str__(self):
         return self.titulo
 
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M Hrs')
+
+    def get_data_criacao(self):
+        return self.data_criacao.strftime('%d/%m/%Y')
+
+
+
 
 
